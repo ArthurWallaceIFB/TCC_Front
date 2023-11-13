7b5d3c5ded6d8222704c9e4d62aa0b39955aed07
artwal@on.sinch.com <arthur.lopes@estudante.ifb.edu.br>
2023-11-13 10:32:25 -0300

    add logger

--- Scraping/Scraping.py
+++ Scraping/Scraping.py
@@ -43,6 +43,12 @@ class IfbSpider(scrapy.Spider):
         super(IfbSpider, self).__init__(*args, **kwargs)
         self.data = []
         self.json_file = open(f"{self.result_file_path}", "w", encoding="utf-8")
+        configure_logging(install_root_handler=False)
+        logging.basicConfig(
+            filename="scrapy.log",
+            format="%(levelname)s: %(message)s",
+            level=logging.DEBUG,
+        )
     
 
     def limpar_texto_html(self, html):
